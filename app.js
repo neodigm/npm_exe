a44971ae15f073a60baba7f0cbb2ce77a9d386dd
Scott Krause <3151842+neodigm@users.noreply.github.com>
2026-07-19 21:30:47 -0500

    rainpath

--- app.js
+++ app.js
@@ -7,6 +7,22 @@ const app = express();
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  const origin = req.get('Origin');
+  if (origin) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, X-Anthropic-API-Key, X-RAI-API-Key, X-RAI-KEY');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+  }
+
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204);
+  }
+
+  next();
+});
+
 app.get('/health', (_req, res) => {
   res.json({ ok: true, service: 'privacy-vault' });
 });
